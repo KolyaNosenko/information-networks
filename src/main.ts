14e7cd3a796a217661ce8f1bc75f6d23e5af9303
Kolya Nosenko <nikolay.nosenko@gen.tech>
2024-09-29 23:48:04 +0300

    feat(views): Add base layout and mock home page

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import * as env from 'dotenv';
+import * as handlebars from 'hbs';
+import * as handlebarsUtils from 'hbs-utils';
 import { join } from 'path';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
@@ -12,6 +14,13 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
+
+  handlebarsUtils(handlebars).registerPartials(
+    join(__dirname, '..', 'views/layout'),
+  );
+  handlebarsUtils(handlebars).registerWatchedPartials(
+    join(__dirname, '..', 'views/layout'),
+  );
 
   await app.listen(process.env.PORT);
 }
