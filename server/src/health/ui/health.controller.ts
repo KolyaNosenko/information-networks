150fa0890af3c81fdc566bf0fb68839c6670c34d
Kolya Nosenko <nikolay.nosenko@gen.tech>
2024-12-03 21:50:15 +0200

    feat(health): Add public to health check

--- server/src/health/ui/health.controller.ts
+++ server/src/health/ui/health.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 import { SuccessResponse } from '../../common/ui/entities';
+import { Public } from 'src/auth/infrastructure';
 
 // TODO use global prefix after moving to API
+@Public()
 @Controller('api/v1/health')
 export class HealthController {
   @Get()
